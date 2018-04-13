ef44dffb46aa90d833d22376ccc2f6f18c3a27c7
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-04-13 20:13:03 +0800

    丑点就先丑点

--- app/src/main/java/cn/bluemobi/dylan/step/activity/FeelingActivity.java
+++ app/src/main/java/cn/bluemobi/dylan/step/activity/FeelingActivity.java
@@ -2,10 +2,23 @@ package cn.bluemobi.dylan.step.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+import cn.bluemobi.dylan.step.R;
 
 public class FeelingActivity extends AppCompatActivity {
+    private String[] data = {"Apple", "Banana",
+    "Orange", "Watermelon", "Pear"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_feeling);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                FeelingActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.list_view_feeling);
+        listView.setAdapter(adapter);
     }
 }
