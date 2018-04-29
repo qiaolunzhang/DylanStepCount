a01d3e33e742abbd892390eb7c4cd6bc4dd99a27
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-04-29 21:51:37 +0800

    锻炼成就页面初步

--- app/src/main/java/cn/bluemobi/dylan/step/activity/AchievementActivity.java
+++ app/src/main/java/cn/bluemobi/dylan/step/activity/AchievementActivity.java
@@ -1,6 +1,14 @@
 package cn.bluemobi.dylan.step.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import cn.bluemobi.dylan.step.R;
+
 public class AchievementActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_achievement);
+    }
 }
